4d297bd0608db905255bc8554ef60c92d6c2f7a6
sinaa <sinaabbaszadeh07@gmail.com>
2023-12-20 21:22:42 +0100

    first commit

--- src/test/java/Phonebook_Test.java
+++ src/test/java/Phonebook_Test.java
@@ -6,6 +6,27 @@
  * Hint: Your test coverage should be 100%
  * Attention:  We would check your commits and it will affect your score
  */
-public class Phonebook_Test {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class Phonebook_Test {  @Test
+public void Persontest(){
+    Person person1=new Person("sina","0933078301");
+    Phonebook phonebook=new Phonebook();
+    phonebook.addContact(person1);
+
+    assertEquals(null, phonebook.getPhoneNumber("sina"));
+
+}  @Test
+public void Persontes0t(){
+    Person person1=new Person("sina","09330783017");
+    Phonebook phonebook=new Phonebook();
+    phonebook.addContact(person1);
+
+    assertEquals("09330783017", phonebook.getPhoneNumber("sina"));
+
+}
+
+
+
 
 }
